9d604c0fb8cc8f60658fb3925a4428ddd6ff2d3d
Tyler <40559762+TylerXNguyen@users.noreply.github.com>
2022-07-20 11:06:03 -0700

    Initial creation of files with basic website display
    	Filled in the structure of the documents and what is needed.
    	The code still requires vastly style and gap improvements,
    	as well as functionality of buttons and calculator.

---
+++ index.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+    return a + b;
+}
+
+function minus(a, b) {
+    return a - b;
+}
+
+function times(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, a, b) {
+    return 0;
+}
+
