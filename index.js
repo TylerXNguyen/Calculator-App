316eac246ae2b896bc4ff64267e389297c34b81a
Tyler <40559762+TylerXNguyen@users.noreply.github.com>
2022-07-20 13:44:11 -0700

    Added functionality evaluating a single expression
        User entering a first number then operator, then the
        second number, and finally the equal sign will yield the correct
        result.

--- index.js
+++ index.js
@@ -1,18 +1,83 @@
 let firstNum = "";
+let secondNum = "";
+let selOperator = "";
+let secondDisplay = "";
 
+const subOutput = document.querySelector(".second-display");
 const output = document.querySelector(".iodisplay");
-const nums = document.querySelectorAll(".nums-btn");
-nums.forEach(num => {
+
+const acBtn = document.getElementById("AC");
+acBtn.addEventListener("click", function() {
+    // All Clear, resets everything
+    firstNum = 0;
+    secondNum = 0;
+    selOperator = "";
+    secondDisplay = "";
+    output.textContent = firstNum;
+    subOutput.textContent = secondDisplay;
+});
+
+const numsButtons = document.querySelectorAll(".nums-btn");
+numsButtons.forEach(num => {
     // Functionality for button numbers
     num.addEventListener("click", function() {
-        if (num.id == "zero") {
-            firstNum += "0";
+        if (subOutput.textContent == "") {
+            // When first number hasn't been submitted
+            if (firstNum == "") {
+                // populating first entry under right conditions
+                if (num.id != "zero") {
+                    firstNum = num.id;
+                }
+            } else {
+                // populating second+ entries
+                if (num.id == "zero") {
+                    firstNum += "0";
+                } else {
+                    firstNum += num.id;
+                }
+            }
+            output.textContent = firstNum;
+            console.log(firstNum)
         } else {
-            firstNum += num.id;
+            // Inputting for second number
+            if (secondNum == "") {
+                // populating first entry under right conditions
+                if (num.id != "zero") {
+                    secondNum = num.id;
+                }
+            } else {
+                // populating second+ entries
+                if (num.id == "zero") {
+                    secondNum += "0";
+                } else {
+                    secondNum += num.id;
+                }
+            }
+            output.textContent = secondNum;
+            console.log(secondNum)
         }
-        output.textContent = firstNum;
+        
     });
 });
+
+const operationsButtons = document.querySelectorAll(".operators button");
+operationsButtons.forEach(operation => {
+    operation.addEventListener("click", function() {
+        // Functionality for operators buttons
+        if (operation.id == "equals") {
+            // When user evaluates expression
+            output.textContent = operate(selOperator, firstNum, secondNum);
+            console.log(operate(selOperator, firstNum, secondNum));
+            console.log(selOperator + " " + firstNum + " " + secondNum);
+        } else {
+            output.textContent = "0";
+            selOperator = operation.id;
+            subOutput.textContent = firstNum + "  " + operation.textContent;
+        }
+    });
+});
+
+
 
 function add(a, b) {
     // adds two numbers a and b
@@ -37,12 +102,13 @@ function divide(a, b) {
 function operate(operator, a, b) {
     // based on the operator,
     // performs operation with two numbers a and b
+    a = Number(a);
+    b = Number(b);
     const operations = {
-        "+" : add(a, b),
-        "-" : minus(a, b),
-        "*" : times(a, b),
-        "/" : divide(a, b),
+        plus : add(a, b),
+        minus : minus(a, b),
+        times : times(a, b),
+        divide : divide(a, b),
     }
-    return operations.operator;
+    return operations[operator];
 }
-
